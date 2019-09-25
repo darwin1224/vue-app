b000e0fa92000a03a2a0a1ed3ebfc6654d48854d
Darwin <darwinblacks@gmail.com>
2019-09-25 22:40:02 +0700

    Remove element ui due to lack support on typescript

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./registerServiceWorker";
-import "./plugins/element";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
@@ -11,4 +10,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
