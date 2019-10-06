f9d9348ccbc96bba8e0d7d50ac3a93478568695c
Darwin <darwinblacks@gmail.com>
2019-10-06 19:18:50 +0700

    Refactor modules

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import 'mutationobserver-shim';
 import Vue from 'vue';
 import './plugins/bootstrap-vue';
 import App from './App.vue';
-import router from './router';
-import store from './store';
+import router from './routers';
+import store from './stores';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
