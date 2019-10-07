acea23cde38daf74415ac74fabe6de98fca86128
Darwin <darwinblacks@gmail.com>
2019-10-07 10:37:38 +0700

    Add post module in store

--- src/stores/modules/Post.ts
+++ src/stores/modules/Post.ts
@@ -1,46 +1,126 @@
-import vm from '@/main';
+import axios from 'axios';
+import { ActionTree, GetterTree, MutationTree, Module } from 'vuex';
+import { PostState, RootState } from '@/types/stores';
+import { PostModel } from '@/types/models';
+import { GET_POST } from '../getter-types';
+import { SET_POST, SET_POST_BY_ID } from '../mutation-types';
+import { getAllPost, getPostById } from '../action-types';
 
 /**
  * State lists
+ *
+ * @type {PostState}
  */
-const state = {
+const state: PostState = {
+  /**
+   * Post list
+   *
+   * @type {PostModel[]}
+   */
   list: []
 };
 
 /**
  * Getter lists
+ *
+ * @type {GetterTree<PostState, RootState>}
  */
-const getters = {
-  GET_POST: (state: any): void => state.list
+const getters: GetterTree<PostState, RootState> = {
+  /**
+   * Get the post list
+   *
+   * @param {PostState} state
+   * @returns {any}
+   */
+  [GET_POST]: (state: PostState): any => state.list
 };
 
 /**
  * Mutation lists
+ *
+ * @type {MutationTree<PostState>}
  */
-const mutations = {
-  SET_POST: (state: any, payload: any): void => (state.list = payload)
+const mutations: MutationTree<PostState> = {
+  /**
+   * Set the payload to the post list
+   *
+   * @param {PostState} state
+   * @param {any} payload
+   * @returns {any}
+   */
+  [SET_POST]: (state: PostState, payload: any): any => {
+    return (state.list = payload);
+  },
+
+  /**
+   * Set the payload the post list by id
+   *
+   * @param {PostState} state
+   * @param {any} payload
+   * @returns {any}
+   */
+  [SET_POST_BY_ID]: (state: PostState, payload: any): any => {
+    return (state.list = payload);
+  }
 };
 
 /**
  * Action lists
+ *
+ * @type {ActionTree<PostState, RootState>}
  */
-const actions = {
-  async getAllPost({ commit }: any): Promise<void> {
-    const { data } = await this.$http.get('http://localhost:3000/post', {
-      headers: {
-        Authorization:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIxMjM0NTYiLCJpYXQiOjE1NzAzNjYxNjIsImV4cCI6MTU3MDM2OTc2Mn0.-59d3si_WT6SIuOap3F_Ez8OT0P2ul1NDidaN39WoxM'
-      }
-    });
-    commit('SET_POST', data);
-    return data;
+const actions: ActionTree<PostState, RootState> = {
+  /**
+   * Get all post data
+   *
+   * @param {any} context.commit
+   * @returns {Promise<PostModel>}
+   */
+  async [getAllPost]({ commit }: any): Promise<PostModel> {
+    try {
+      const { data } = await axios.get('http://localhost:3000/post');
+      commit('SET_POST', data);
+      return data;
+    } catch (err) {
+      return err;
+    }
+  },
+
+  /**
+   * Get post data by id
+   *
+   * @param {any} context.commit
+   * @returns {Promise<PostModel>}
+   */
+  async [getPostById]({ commit }: any): Promise<PostModel> {
+    try {
+      const { data } = await axios.get('http://localhost:3000/post/1');
+      commit('SET_POST_BY_ID', data);
+      return data;
+    } catch (err) {
+      return err;
+    }
   }
 };
 
-export default {
-  namespaced: true,
+/**
+ * Namespaced state
+ *
+ * @type {boolean}
+ */
+const namespaced: boolean = true;
+
+/**
+ * Post module
+ *
+ * @type {Module<PostState, RootState>}
+ */
+const post: Module<PostState, RootState> = {
+  namespaced,
   state,
   getters,
   mutations,
   actions
 };
+
+export default post;
