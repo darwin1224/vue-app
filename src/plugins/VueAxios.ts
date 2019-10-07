73fe326b7a7da8145740a4744f7ed6b06a49fc8c
Darwin <darwinblacks@gmail.com>
2019-10-07 14:20:05 +0700

    Refactor codes for VueAxios plugin

--- src/plugins/VueAxios.ts
+++ src/plugins/VueAxios.ts
@@ -1,6 +1,22 @@
 import _Vue from 'vue';
-import Axios from 'axios';
+import axios from 'axios';
 
-export function VueAxios(Vue: typeof _Vue, options?: any): void {
-  Vue.prototype.$http = Axios;
-}
+/**
+ * Axios baseURL
+ *
+ * @type {string}
+ */
+axios.defaults.baseURL = 'http://localhost:3000';
+
+export default {
+  /**
+   * Axios plugin
+   *
+   * @param {_Vue} Vue
+   * @param {any} options
+   * @returns {void}
+   */
+  install(Vue: typeof _Vue, options?: any): void {
+    Vue.prototype.$http = axios;
+  }
+};
