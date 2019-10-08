9be61e7d3e18706568db770e60e07a8802243264
Darwin <darwinblacks@gmail.com>
2019-10-08 10:02:38 +0700

    Import index.scss rather import some of bootstrap files in bootstrap-vue.ts

--- src/plugins/bootstrap-vue.ts
+++ src/plugins/bootstrap-vue.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 
 import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import '@/assets/scss/index.scss';
 
 Vue.use(BootstrapVue);
