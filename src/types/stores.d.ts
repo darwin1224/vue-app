55929d92f451c1080c14c9d7897de7f353da58b9
Darwin <darwinblacks@gmail.com>
2019-10-08 16:19:29 +0700

    Add GetById and UpdateById types in stores.d.ts

--- src/types/stores.d.ts
+++ src/types/stores.d.ts
@@ -1,9 +1,26 @@
-import { PostModel, AuthModel } from './models';
+import Vue from 'vue';
+import { PostModel, AuthModel, ObjectId } from './models';
 
 /**
  * Root state in store
  */
 export interface RootState {}
+
+/**
+ * Get by id action types
+ */
+export interface GetById {
+  id: ObjectId;
+  vm: Vue;
+}
+
+/**
+ * Update data by id types
+ */
+export interface UpdateById {
+  id: ObjectId;
+  params: PostModel;
+}
 
 /**
  * Post state
@@ -15,6 +32,13 @@ export interface PostState {
    * @type {PostModel[]}
    */
   list: PostModel[];
+
+  /**
+   * List of post by id
+   *
+   * @type {PostModel}
+   */
+  listById: PostModel;
 }
 
 /**
